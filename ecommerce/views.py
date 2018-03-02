86da36dfbcf0c959745c1ef3515fd3c5795cff4f
erindy <erindmehmeti@gmail.com>
2018-03-03 00:14:50 +0100

    Added views 2

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import LoginForm
 
 
 def home_page(request):
-    return HttpResponse("Hello World")
+    return render(request, "home_page.html")
+
+def about_page(request):
+    return render(request, "home_page.html")
+
+
+def contact_page(request):
+
+    if request.method == "POST":
+        print(request.POST.get('fullname'))
+    context = {
+        'title': "Contact",
+        "content": "Welcome to the contact page."
+    }
+    return render(request, "contact/view.html", context)
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request, "auth/login.html", {})
+
+def register_page(request):
+    return render(request, "auth/login.html", {})
+
+
