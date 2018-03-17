11a8d8428203658f3824300fc3a3726410f1b9cd
erindy <erindmehmeti@gmail.com>
2018-03-17 14:58:14 +0100

    added 004

--- products/models.py
+++ products/models.py
@@ -1,4 +1,22 @@
+import random
+import os
 from django.db import models
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+
+    return name, ext
+
+
+
+def upload_image_path(instance, filename):
+    print(instance)
+    print(filename)
+    new_filename =random.randint(1, 265059590)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return 'products/{new_filename}/{final_filename}'.format(new_filename=new_filename, final_filename=final_filename)
 
 # Create your models here.
 
@@ -6,6 +24,7 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 
     def __str__(self):
         return self.title
